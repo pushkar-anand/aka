7638323a01a8c54677ed7427a8d686388f440194
Pushkar Anand <anandpushkar088@gmail.com>
2018-10-03 19:12:27 +0530

    finished.

--- main.go
+++ main.go
@@ -1,7 +1,47 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+)
+
+func check(e error) {
+	if e != nil {
+		panic(e)
+	}
+}
 
 func main() {
-	fmt.Println("Hello, world.")
+
+	args := os.Args[1:]
+
+	if len(args) >= 2 {
+
+		alias := args[0]
+		cmd := args[1]
+
+		ln := "alias " + alias + "='" + cmd + "' "
+		println("append: " + ln)
+
+		home := os.Getenv("HOME")
+		println("HOME: " + home)
+
+		bashRcFileLocation := home + "/.bashrc"
+
+		bashRcFile, err := os.OpenFile(bashRcFileLocation, os.O_APPEND|os.O_WRONLY, 0644)
+		check(err)
+
+		defer bashRcFile.Close()
+
+		writeToBashRc := "#ADDED BY bashAliasCreator \n" + ln + "\n"
+		fmt.Println(writeToBashRc)
+
+		if _, err = bashRcFile.WriteString(writeToBashRc); err != nil {
+			panic(err)
+		}
+
+	} else {
+		println("Incorrect Arguments. Use as bashAliasCreator alias \"command\"  ")
+	}
+
 }
